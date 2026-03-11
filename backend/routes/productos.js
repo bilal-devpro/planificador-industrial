08fbaf19fc1b5fd0f464cfa3ae33dc2381bdf4dc
bilal <bilal.devpro@gmail.com>
2026-03-11 13:06:06 +0100

    solucionarproblemas 0.8 rutas

--- backend/routes/productos.js
+++ backend/routes/productos.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../database');
+const { pool } = require('../database');
 
 router.get('/', async (req, res) => {
   const result = await pool.query('SELECT * FROM productos');
